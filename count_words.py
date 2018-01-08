5feda85025441bb41bc61611451e11e5eed20adc
ManiaJack <hzwxh@vip.qq.com>
2018-01-08 11:34:02 +0800

    py.checkio.org猴子打字问题。正则匹配。

---
+++ count_words.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+'传入字符串+关键词，匹配字符串中出现的关键词，返回出现的关键词的个数（无重复）。'
+
+__author__ = 'ManiaJack'
+
+
+import re
+
+
+def count_words(strings, keywords):
+    word_number = 0
+    for words in keywords:
+        re_rule = '.*' + words
+        if re.match(re_rule, strings.lower()):
+            word_number += 1
+    return word_number
